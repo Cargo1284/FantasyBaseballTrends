407dabdcc15548394d376c0ed33448d0f8df504e
Carlos G <cargo@Carloss-MacBook-Pro-5.local>
2023-04-06 01:02:18 -0400

    changes to rst

--- docs/conf.py
+++ docs/conf.py
@@ -40,3 +40,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_use_param = False
+napoleon_use_rtype = False
