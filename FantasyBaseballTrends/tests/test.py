82e61ee0e8a0bddf4afe221eb06fd19b55749803
Carlos G <cargo@Carloss-MacBook-Pro-5.local>
2023-05-10 00:52:19 -0400

    added new test suite

--- FantasyBaseballTrends/tests/test.py
+++ FantasyBaseballTrends/tests/test.py
@@ -17,32 +17,32 @@ class TestFunctions(unittest.TestCase):
     # test to see if the projected stats exist and returns data Frame
     def test_get2023Season(self):
         self.assertEqual(
-            type(fbt.get2023Season(self.driver, "Aaron Judge")),
+            type(fbt.get2023Season(self.driver, "Yadier Molina")),
             pd.DataFrame,
         )
 
     def test_getCareer(self):
         self.assertEqual(
-            type(fbt.getCareer(self.driver, "Aaron Judge")),
+            type(fbt.getCareer(self.driver, "Yadier Molina")),
             pd.DataFrame,
         )
 
     def test_getPostseasonStats(self):
         self.assertEqual(
-            type(fbt.getPostseasonStats(self.driver, "Aaron Judge")),
+            type(fbt.getPostseasonStats(self.driver, "Yadier Molina")),
             pd.DataFrame,
         )
 
     # test to see if last game stats exist
     def test_getLastGame(self):
         self.assertEqual(
-            type(fbt.getLastGame(self.driver, "Aaron Judge")), pd.DataFrame
+            type(fbt.getLastGame(self.driver, "Yadier Molina")), pd.DataFrame
         )
 
     # assert that only one row is returned
     def test_getLastGameShape(self):
         self.assertEqual(
-            fbt.getLastGame(self.driver, "Mike Trout").shape[0], 1
+            fbt.getLastGame(self.driver, "Yadier Molina").shape[0], 1
         )
 
     # test to see if Rhp Current stats exist
@@ -65,76 +65,76 @@ class TestFunctions(unittest.TestCase):
 
     def test_intget2023Season(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/AaronJudgeSeason.csv', index_col=0
+            'FantasyBaseballTrends/tests/MolinaSeason.csv', index_col=0
         )
         assert_frame_equal(
-            fbt.get2023Season(self.driver, "Aaron Judge"),
+            fbt.get2023Season(self.driver, "Yadier Molina"),
             testFrame,
             check_dtype=False,
         )
 
     def test_intgetLastGame(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/JudgeLastGame.csv',
+            'FantasyBaseballTrends/tests/MolinaLastGame.csv',
             index_col=0,
             dtype=str,
         )
         testFrame = testFrame.astype(str)
-        actualFrame = fbt.getLastGame(self.driver, "Aaron Judge")
+        actualFrame = fbt.getLastGame(self.driver, "Yadier Molina")
         actualFrame = actualFrame.astype(str)
         assert_frame_equal(actualFrame, testFrame, check_dtype=False)
 
     def test_intgetPostseason(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/AaronJudgePostseason.csv', index_col=0
+            'FantasyBaseballTrends/tests/MolinaPostseason.csv', index_col=0
         )
         testFrame = testFrame.astype(str)
-        actualFrame = fbt.getPostseasonStats(self.driver, "Aaron Judge")
+        actualFrame = fbt.getPostseasonStats(self.driver, "Yadier Molina")
         actualFrame = actualFrame.astype(str)
         assert_frame_equal(actualFrame, testFrame, check_dtype=False)
 
     def test_intgetCareer(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/AaronJudgeCareer.csv', index_col=0
+            'FantasyBaseballTrends/tests/MolinaCareer.csv', index_col=0
         )
         assert_frame_equal(
-            fbt.getCareer(self.driver, "Aaron Judge"),
+            fbt.getCareer(self.driver, "Yadier Molina"),
             testFrame,
             check_dtype=False,
         )
 
     def test_intgetRhpCurrent(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/JudgeRhpCurrent.csv', index_col=0
+            'FantasyBaseballTrends/tests/MolinaRhpCurrent.csv', index_col=0
         )
         assert_frame_equal(
-            fbt.getVsRhpCurrent(self.driver, "Aaron Judge"), testFrame
+            fbt.getVsRhpCurrent(self.driver, "Yadier Molina"), testFrame
         )
 
     def test_intgetLhpCurrent(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/JudgeLhpCurrent.csv', index_col=0
+            'FantasyBaseballTrends/tests/MolinaLhpCurrent.csv', index_col=0
         )
         assert_frame_equal(
-            fbt.getVsLhpCurrent(self.driver, "Aaron Judge"), testFrame
+            fbt.getVsLhpCurrent(self.driver, "Yadier Molina"), testFrame
         )
 
     def test_intgetCarrerSplits(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/JudgeCarrerSplits.csv', index_col=0
+            'FantasyBaseballTrends/tests/MolinaCarrerSplits.csv', index_col=0
         )
         assert_frame_equal(
-            fbt.getCareerSplits(self.driver, "Aaron Judge"), testFrame
+            fbt.getCareerSplits(self.driver, "Yadier Molina"), testFrame
         )
 
     def test_intgetLastxGames1(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/JudgeLast5.csv',
+            'FantasyBaseballTrends/tests/MolinaLast5.csv',
             index_col=0,
             dtype=str,
         )
         testFrame = testFrame.astype(str)
-        actualFrame = fbt.getLastxGames(self.driver, "Aaron Judge", 5)
+        actualFrame = fbt.getLastxGames(self.driver, "Yadier Molina", 5)
         actualFrame = actualFrame.astype(str)
         assert_frame_equal(
             actualFrame,
@@ -144,12 +144,12 @@ class TestFunctions(unittest.TestCase):
 
     def test_intgetLastxGames2(self):
         testFrame = pd.read_csv(
-            'FantasyBaseballTrends/tests/JudgeLast10.csv',
+            'FantasyBaseballTrends/tests/MolinaLast10.csv',
             index_col=0,
             dtype=str,
         )
         testFrame = testFrame.astype(str)
-        actualFrame = fbt.getLastxGames(self.driver, "Aaron Judge", 10)
+        actualFrame = fbt.getLastxGames(self.driver, "Yadier Molina", 10)
         actualFrame = actualFrame.astype(str)
         assert_frame_equal(
             actualFrame,
